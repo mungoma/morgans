63a1fcbcea019d45e535dfbbc5d1e3b9c30a5fae
mungoma <mungomakevin256@gmail.com>
2021-06-15 19:27:02 +0300

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <div class="header bg-gradient-primary py-7 py-lg-8">
         <div class="container">
-                    <div class="row">
+                <div class="row">
                     <div class="col-sm">
 
                     <div class="card" style="width: 18rem;">
                         <img class="card-img-top" src="../../assets/img/theme/img-1-1000x600.jpg" alt="Card image cap">
                         <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Entebbe Apartments</h5>
+                        <p class="card-text">Amazing Apartments built just for you to make a perfect Home.</p>
+                        <a href="#" class="btn btn-primary">More Information</a>
                         </div>
                     </div>
 
@@ -19,31 +19,29 @@
                     </div>
                     <div class="col-sm">
 
-
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="../../assets/img/theme/img-1-1000x600.jpg" alt="Card image cap">
-                        <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../../assets/img/theme/img-1-1000x600.jpg" alt="Card image cap">
+                            <div class="card-body">
+                            <h5 class="card-title">Entebbe Apartments</h5>
+                            <p class="card-text">Amazing Apartments built just for you to make a perfect Home.</p>
+                            <a href="#" class="btn btn-primary">More Information</a>
+                            </div>
                         </div>
                     </div>
-
-
-                    </div>
                     <div class="col-sm">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="../../assets/img/theme/img-1-1000x600.jpg" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="../../assets/img/theme/img-1-1000x600.jpg" alt="Card image cap">
+                            <div class="card-body">
+                            <h5 class="card-title">Entebbe Apartments</h5>
+                            <p class="card-text">Amazing Apartments built just for you to make a perfect Home.</p>
+                            <a href="#" class="btn btn-primary">More Information</a>
+                            </div>
+                        </div>
 
 
+
                     </div>
-                    </div>
+            </div>
 
 
         </div>
